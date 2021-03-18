09dc1646b1c7440e46029bb56bcbad8d6bc22cca
langtonfavor <langtonfavor@gmail.com>
2021-03-18 10:55:20 +0200

    changed uri

--- server/server.js
+++ server/server.js
@@ -11,6 +11,17 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 // Express: Listener
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'src/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src /build', 'index.html'));
+  });
+}
+
+
 app.listen(PORT, () => {
   console.log(`The server has started on port: ${PORT}`);
 });
